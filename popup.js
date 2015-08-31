f655c09a61284d08701c973a682b756914497932
wucangji <wucangji@gmail.com>
2015-08-31 00:38:27 -0400

    0.0.2 support imdb rating
    add a image to make sure the movie is right

--- popup.js
+++ popup.js
@@ -34,10 +34,27 @@ $(function () {
         });
     });
 
+    function getScoreFromIMDB () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://www.omdbapi.com/?t=" + arguments[0], true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                // JSON.parse does not evaluate the attacker's scripts.
+                var resp = JSON.parse(xhr.responseText);
+                console.log( resp );
+                console.log( resp.imdbRating);
+                var rating = resp.imdbRating;
+                $('#imdbRating').text(rating);
+            }
+        };
+        xhr.send();
+    }
+
+
     $('#Search').click(function () {
 
         var movieName = $('#MovieName').val();
-        console.log(movieName);
+
 
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "https://api.douban.com/v2/movie/search?q= " + movieName + "&count=1", true);
@@ -48,10 +65,23 @@ $(function () {
                 console.log( resp );
                 console.log( resp.subjects[0].rating.average);
                 var rating = resp.subjects[0].rating.average;
+                var DoubanName1 = resp.subjects[0].title;
+                console.log(resp.subjects[0].original_title);
+                $('#Name1').text(DoubanName1);
+                movieName = resp.subjects[0].original_title;
+                $('#Name2').text(resp.subjects[0].original_title +" : " + resp.subjects[0].year);
+                // ToDO: if there is image, remove them first
+                // add fly image.
+                $( "<img>" ).attr( "src", resp.subjects[0].images.medium ).appendTo( "#images" );
                 $('#doubanRating').text(rating);
+
+                // must put inside this http get, otherwise moviename is not change
+                getScoreFromIMDB(movieName);
             }
         };
         xhr.send();
+
+
 
     });
 
